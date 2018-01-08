e2651f3de0ef20781a47285a471fff3c5a8a24c5
Syed_Bakhtiyar <syedbakhtiyar120@gmail.com>
2018-01-08 17:58:07 +0500

    Expense Activity'

---
+++ app/src/main/java/bilal/com/captain/expenceActivity/ExpenseActivity.java
@@ -0,0 +1,15 @@
+package bilal.com.captain.expenceActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import bilal.com.captain.R;
+
+public class ExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense);
+    }
+}
