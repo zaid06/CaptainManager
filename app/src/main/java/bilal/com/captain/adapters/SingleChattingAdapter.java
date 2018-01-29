f1fc0b6ec5694265da78225bd442ced71e17c821
shameel96 <sshameelakhtar@gmail.com>
2018-01-30 03:18:46 +0500

    Download image work in progress

--- app/src/main/java/bilal/com/captain/adapters/SingleChattingAdapter.java
+++ app/src/main/java/bilal/com/captain/adapters/SingleChattingAdapter.java
@@ -1,14 +1,21 @@
 package bilal.com.captain.adapters;
 
+import android.app.DownloadManager;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -17,10 +24,18 @@ import android.widget.LinearLayout;
 import com.bumptech.glide.Glide;
 
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 
 import bilal.com.captain.ChatGlobal;
 import bilal.com.captain.R;
+import bilal.com.captain.chatActivity.StartOneToOneChatting;
 import bilal.com.captain.classes.BoldCustomTextView;
 import bilal.com.captain.classes.RegularCustomTextView;
 import bilal.com.captain.galleryActivity.GalleryActivity;
@@ -51,7 +66,7 @@ public class SingleChattingAdapter extends ArrayAdapter<SingleChatModel> {
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
 
 
@@ -83,6 +98,8 @@ public class SingleChattingAdapter extends ArrayAdapter<SingleChatModel> {
                         return false;
                     }
                 });
+
+
 
             }else {
 
@@ -119,6 +136,18 @@ public class SingleChattingAdapter extends ArrayAdapter<SingleChatModel> {
                 });
 
 
+                viewHolder.gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                       String url = (String
+                               ) adapterView.getItemAtPosition(i);
+                        openAlert(position,url);
+                        return false;
+
+                    }
+                });
+
+
             }else {
 
                 viewHolder.linearLayout.setVisibility(View.GONE);
@@ -131,6 +160,111 @@ public class SingleChattingAdapter extends ArrayAdapter<SingleChatModel> {
 
 
         return convertView;
+    }
+
+    private void openAlert(final int position, final  String url) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(context);
+        View view = inflater.inflate(R.layout.imagedownload,null,false);
+
+        final LinearLayout yes = (LinearLayout) view.findViewById(R.id.downloadyes);
+        final LinearLayout no = (LinearLayout) view.findViewById(R.id.downloadno);
+
+        alert.setView(view);
+        final AlertDialog dialog = alert.create();
+
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogTheme;
+        dialog.show();
+
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                downloadImages(url);
+                DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+
+
+               Uri imageuri = url;
+
+                DownloadManager.Request request = new DownloadManager.Request(imageuri);
+
+                request.setTitle("Download Images");
+
+                request.setDescription("Downloading..");
+
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+                request.setDestinationUri(Uri.parse(Environment.getExternalStorageDirectory() + "/Captain/Download Images"));
+
+                downloadManager.enqueue(request);
+            }
+        });
+    }
+
+    private void downloadImages(String imageUrl){
+
+        try {
+//            java.net.URL url = new URL(imageUrl);
+
+            // TODO Bakhtiyar: I create this because some images are correct in server but Java think its corrupt
+
+            boolean success = false;
+
+            int pos = imageUrl.lastIndexOf('/') + 1;
+
+            URI uri = null;
+
+            try {
+
+                uri = new URI(imageUrl.substring(0, pos) + Uri.encode(imageUrl.substring(pos)));
+
+                Log.d("uri", "onClick: " + uri);
+
+            } catch (Exception e) {
+                Log.d("exc", "onClick: " + e + uri);
+            }
+
+
+            java.net.URL url = new URL(String.valueOf(uri));
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+
+            String name = imageUrl.substring(String.valueOf(imageUrl).lastIndexOf("/") + 1);
+
+            File file = null;
+
+            File myDir = new File(Environment.getExternalStorageDirectory() + "/Captain/Download Images" );
+            if (myDir.exists()) {
+                success = true;
+            } else {
+                success = myDir.mkdirs();
+            }
+
+            if (success) {
+
+                file = new File(myDir, name);
+                if (file.exists())
+                    file.delete();
+                try {
+                    FileOutputStream out = new FileOutputStream(file);
+                    myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+                    out.flush();
+                    out.close();
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+
     }
 
     static class ViewHolder{
